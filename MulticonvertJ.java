c65886acc0ab1ee3bca1d3b8a14cfce941e97cce
Skyler <50990788+TheSkyler-Dev@users.noreply.github.com>
2024-10-09 13:34:25 +0200

    v0.2.0-alpha source code
    The updated source code for MulticonvertJE

--- MulticonvertJ.java
+++ MulticonvertJ.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
 import java.util.Locale;
-//MulticonvertJ v0.1.0-alpha (c) Innokenti "Skyler" Nikiforov
+//MulticonvertJE v0.2.0-alpha (c) Innokenti "Skyler" Nikiforov
 class MulticonvertJ {
     public static void main(String[] args){
         while (true) { 
